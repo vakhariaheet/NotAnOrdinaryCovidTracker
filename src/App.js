ce0968118354d2816fedd212848c945fbf49cf26
Heet Vakharia <heet1476@gmail.com>
2020-08-15 00:32:54 +0530

    Added Map Component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+import Map from "./Components/Map/map.component";
+import "./App.css";
+
+const App = () => {
+  const [selectedCountry, setSelectedCountry] = useState("");
+  setInterval(function () {
+    // Set interval for checking
+    var date = new Date(); // Create a Date object to find out what time it is
+    if (date.getHours() === 0 && date.getMinutes() === 0) {
+      // Check the time
+      // Do stuff
+    }
+  }, 60000); // Repeat every 60000 milliseconds (1 minute)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Map setSelectedCountry={setSelectedCountry} />
+      <h1>{selectedCountry} </h1>
     </div>
   );
-}
+};
 
 export default App;
