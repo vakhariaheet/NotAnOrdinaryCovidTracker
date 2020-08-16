5eb493eeeca6cbee1f6021b1352b3e5ef2051eeb
Heet Vakharia <heet1476@gmail.com>
2020-08-16 19:09:30 +0530

    App Completed, Media Queries and animation left

--- src/Components/DailyCases/DailyCases.component.jsx
+++ src/Components/DailyCases/DailyCases.component.jsx
@@ -1,12 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./DailyCases.styles.scss";
 
 const DailyCases = () => {
-  const [activeCases, setActiveCases] = useState(0);
+  const [criticalCases, setCriticalCases] = useState(0);
   const [confirmedCases, setConfirmedCases] = useState(0);
   const [recoveredCases, setRecoveredCases] = useState(0);
   const [deathCases, setDeathCases] = useState(0);
-  //   active: 5361549
+  useEffect((criticalCases, deathCases, recoveredCases, confirmedCases) => {
+    fetch("https://covid-19-data.p.rapidapi.com/totals?format=json", {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "covid-19-data.p.rapidapi.com",
+        "x-rapidapi-key": "59d0b2c3c0msh86e340ae9c71bb8p13d7cbjsn0c90670232b6",
+      },
+    })
+      .then((response) => response.json())
+      .then((stats) => {
+        const { critical, confirmed, deaths, recovered } = stats[0];
+        setCriticalCases(critical);
+        setConfirmedCases(confirmed);
+        setRecoveredCases(recovered);
+        setDeathCases(deaths);
+        console.log(stats);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    return undefined;
+  }, []);
+  // Repeat every 60000 milliseconds (2 minute)
+  //   critical: 5361549
   // confirmed: 15085083
   // critical: 63786
   // date: "2020-07-21"
@@ -15,7 +38,7 @@ const DailyCases = () => {
   return (
     <div className="dailycases">
       <h1 className="dailycases__heading">Global</h1>
-      <div className="dailycases__active dailycases__item">
+      <div className="dailycases__critical dailycases__item">
         <svg
           className="dailycases__item_svg"
           xmlns="http://www.w3.org/2000/svg"
@@ -27,8 +50,8 @@ const DailyCases = () => {
             d="M624,352H611.88c-28.51,0-42.79-34.47-22.63-54.63l8.58-8.57a16,16,0,1,0-22.63-22.63l-8.57,8.58C546.47,294.91,512,280.63,512,252.12V240a16,16,0,0,0-32,0v12.12c0,28.51-34.47,42.79-54.63,22.63l-8.57-8.58a16,16,0,0,0-22.63,22.63l8.58,8.57c20.16,20.16,5.88,54.63-22.63,54.63H368a16,16,0,0,0,0,32h12.12c28.51,0,42.79,34.47,22.63,54.63l-8.58,8.57a16,16,0,1,0,22.63,22.63l8.57-8.58c20.16-20.16,54.63-5.88,54.63,22.63V496a16,16,0,0,0,32,0V483.88c0-28.51,34.47-42.79,54.63-22.63l8.57,8.58a16,16,0,1,0,22.63-22.63l-8.58-8.57C569.09,418.47,583.37,384,611.88,384H624a16,16,0,0,0,0-32ZM480,384a32,32,0,1,1,32-32A32,32,0,0,1,480,384ZM346.51,213.33h16.16a21.33,21.33,0,0,0,0-42.66H346.51c-38,0-57.05-46-30.17-72.84l11.43-11.44A21.33,21.33,0,0,0,297.6,56.23L286.17,67.66c-26.88,26.88-72.84,7.85-72.84-30.17V21.33a21.33,21.33,0,0,0-42.66,0V37.49c0,38-46,57.05-72.84,30.17L86.4,56.23A21.33,21.33,0,0,0,56.23,86.39L67.66,97.83c26.88,26.88,7.85,72.84-30.17,72.84H21.33a21.33,21.33,0,0,0,0,42.66H37.49c38,0,57.05,46,30.17,72.84L56.23,297.6A21.33,21.33,0,1,0,86.4,327.77l11.43-11.43c26.88-26.88,72.84-7.85,72.84,30.17v16.16a21.33,21.33,0,0,0,42.66,0V346.51c0-38,46-57.05,72.84-30.17l11.43,11.43a21.33,21.33,0,0,0,30.17-30.17l-11.43-11.43C289.46,259.29,308.49,213.33,346.51,213.33ZM160,192a32,32,0,1,1,32-32A32,32,0,0,1,160,192Zm80,32a16,16,0,1,1,16-16A16,16,0,0,1,240,224Z"
           ></path>
         </svg>
-        <h2 className="dailycases__active--text dailycases__item--text">
-          Active : {activeCases}
+        <h2 className="dailycases__critical--text dailycases__item--text">
+          Active : {criticalCases}
         </h2>
       </div>
       <div className="dailycases__confirmed dailycases__item">
