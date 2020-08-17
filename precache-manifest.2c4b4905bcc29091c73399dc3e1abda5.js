97ef8fa5efb2e67407612f568fa43c7d1130e274
Heet Vakharia <heet1476@gmail.com>
2020-08-17 22:35:22 +0530

    Updates

---
+++ precache-manifest.2c4b4905bcc29091c73399dc3e1abda5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd73cca100b90b08e51b25c6e5e98a60",
+    "url": "/NotAnOridinaryCovidTracker/index.html"
+  },
+  {
+    "revision": "25ab9be7834b323a528a",
+    "url": "/NotAnOridinaryCovidTracker/static/css/main.806cc2e6.chunk.css"
+  },
+  {
+    "revision": "8d3128818d84ced34768",
+    "url": "/NotAnOridinaryCovidTracker/static/js/2.5b6c8379.chunk.js"
+  },
+  {
+    "revision": "9c09199232d0dd905a2c33067c073e3f",
+    "url": "/NotAnOridinaryCovidTracker/static/js/2.5b6c8379.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25ab9be7834b323a528a",
+    "url": "/NotAnOridinaryCovidTracker/static/js/main.269ae79b.chunk.js"
+  },
+  {
+    "revision": "5b9bc18d5a00a6883927",
+    "url": "/NotAnOridinaryCovidTracker/static/js/runtime-main.7bd25eeb.js"
+  },
+  {
+    "revision": "ab91ca93c2ecfc64e3c499fbea228dbc",
+    "url": "/NotAnOridinaryCovidTracker/static/media/background.ab91ca93.jpeg"
+  }
+]);
